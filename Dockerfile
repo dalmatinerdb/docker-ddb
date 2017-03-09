c0ea184c31a0cd2660bd5c01b2b6d4367ded7ca7
Heinz N. Gies <heinz@licenser.net>
2017-03-09 04:57:25 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM erlang:19.2.3
+MAINTAINER Heinz N. Gies <heinz@project-fifo.net>
+
+###################
+##
+## Get DalmatinerFE
+##
+###################
+
+ENV DDB_VSN=0.3.1
+ENV DDB_PATH=/dalmatinerdb
+ENV DDB_REF=$DDB_VSN
+
+RUN cd / \
+    && env GIT_SSL_NO_VERIFY=true git clone -b $DDB_VSN http://github.com/dalmatinerdb/dalmatinerdb.git dalmatinerdb.git
+
+RUN cd dalmatinerdb.git \
+    && env GIT_SSL_NO_VERIFY=true git checkout $DDB_REF \
+    && make rel \
+    && mv /dalmatinerdb.git/_build/prod/rel/ddb $DDB_PATH \
+    && rm -rf /dalmatinerdb.git \
+    && rm -rf $DDB_PATH/lib/*/c_src
+
+RUN mkdir -p /data/dalmatinerdb/etc \
+    && mkdir -p /data/dalmatinerdb/db \
+    && mkdir -p /data/dalmatinerdb/log \
+    && cp $DDB_PATH/etc/dalmatinerdb.conf.example /data/dalmatinerdb/etc/dalmatinerdb.conf \
+    && sed -i -e '/RUNNER_USER=dalmatiner/d' $DDB_PATH/bin/ddb \
+    && sed -i -e '/RUNNER_USER=dalmatiner/d' $DDB_PATH/bin/ddb-admin
+
+COPY ddb.sh /
+
+EXPOSE 5555
+
+ENTRYPOINT ["/ddb.sh"]
---
+++ ddb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+cat >&2 <<-'EOWARN'
+	****************************************************
+                   The following ports / services
+                     * 5555 / dalmatiner DB
+	****************************************************
+EOWARN
+
+CONF=/data/dalmatinerdb/etc/dalmatinerdb.conf
+
+set -x
+
+export HOST=$(ping -c1 $HOSTNAME | awk '/^PING/ {print $3}' | sed 's/[():]//g')||'127.0.0.1'
+
+export CLUSTER_NAME=${CLUSTER_NAME:-ddb}
+
+export COORDINATOR_NODE=${COORDINATOR_NODE:-$HOSTNAME}
+
+export COORDINATOR_NODE_HOST=$(ping -c1 $COORDINATOR_NODE | awk '/^PING/ {print $3}' | sed 's/[():]//g')||'127.0.0.1'
+
+
+export RING_SIZE=${RING_SIZE:-64}
+
+
+## Update config
+sed -i \
+    -e "s/^nodename = .*/nodename = ${CLUSTER_NAME}@${HOST}/" \
+    -e "s/^handoff.ip = .*/handoff.ip = ${HOST}/" \
+    $CONF
+
+echo "ring_size = ${RING_SIZE}" >> $CONF
+
+
+#set +x
+
+admin=/dalmatinerdb/bin/ddb-admin
+ddb=/dalmatinerdb/bin/ddb
+
+$ddb start
+$admin wait-for-service metric
+$admin wait-for-service event
+
+if [ ! "${COORDINATOR_NODE_HOST}" = "${HOST}" ]
+then
+    $admin cluster join ${CLUSTER_NAME}@${COORDINATOR_NODE_HOST}
+    sleep $(( ( RANDOM % 10 )  + 5 ))
+    $admin cluster plan
+    $admin cluster commit
+fi
+
+tail -n 1024 -f /data/dalmatinerdb/log/console.log
